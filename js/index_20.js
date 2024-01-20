5f3a655f6a7f11b9e8a1e8050b9ec5cbdf204eac
Ihor-Protskolli <ProtsenkoIhor@proton.me>
2024-01-20 14:03:06 +0200

    For loops

---
+++ js/index_20.js
@@ -0,0 +1,14 @@
+// for loop = repeat some code a LIMITED count of times
+
+// for (let i = 10; i > 0; i -= 2) {
+//   console.log(i);
+// }
+// console.log("HAPPY NEW YEAR!");
+
+for (i = 1; i <= 20; i++) {
+  if (i == 13) {
+    // continue;
+    break;
+  }
+  console.log(i);
+}
