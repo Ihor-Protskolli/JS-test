1dc7dfdf1600637cee1b4e1795d5247b88062a33
Ihor <ProtsenkoIhor@proton.me>
2024-03-09 17:43:27 +0200

    DOM navigation (update)

--- js/index_61.js
+++ js/index_61.js
@@ -1,5 +1,28 @@
 // DOM navigation = Process of navigating through HTML using JS
 
+// !!!!!! HTML !!!!!!
+
+    
+      <ul id="fruits">
+        <li id="apple">Apple</li>
+        <li id="banana">Banana</li>
+        <li id="orange">Orange</li>
+      </ul>
+
+      <ul id="vegetables">
+        <li id="carrots">Carrots</li>
+        <li id="potatoes">Potatoes</li>
+        <li id="onions">Onions</li>
+      </ul>
+      
+      <ul id="desserts">
+        <li id="cake">cake</li>
+        <li id="pie">pie</li>
+        <li id="ice-cream">ice cream</li>
+      </ul>
+
+      
+
 //  ------------------ .firstElementChild ------------------
  
 // const element = document.getElementById("fruits");
